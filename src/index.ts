f20632d3e8c2eafad5bc256e5e06dacb54ce6ee3
Adanosz <reti925@gmail.com>
2020-04-23 10:40:52 +0200

    controllers and models added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import * as express from 'express';
 import { Request, Response, Application } from 'express';
 import { database } from './lib/database';
+import {User} from './app/models/user';
+import {Group} from './app/models/group';
+import * as userController from './app/controllers/user'
+import * as groupController from './app/controllers/group'
 
 const app: Application = express();
 app.use(express.json())
 const { PORT = 3000 } = process.env;
 
-interface User {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  age: number;
-  created_at?: string;
-  updated_at?: string;
-}
 
 app.get('/', async (req: Request, res: Response) => {
   res.json({
@@ -22,171 +17,42 @@ app.get('/', async (req: Request, res: Response) => {
   });
 });
 
+
 /*************USERS*************/
 
 //index
-app.get('/user', async (req: Request, res: Response) => {
-  const users: Array<User> = await database('users').select();
-  res.json(users);
-});
+app.get('/user', userController.index);
 
 //show
-app.get('/user/:id', async (req: Request, res: Response) => {
-  try {
-    const user: User = await database('users').select().where({ id: req.params.id }).first();
-    if (user) {
-      res.json(user);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.get('/user/:id', userController.show);
 
 //create
-app.post('/user', async (req: Request, res: Response) => {
-  try {
-    const user: User = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      age: req.body.age
-    }
-    await database('users').insert(user);
-    res.sendStatus(201);
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
-
-//delete
-app.delete('/user/:id', async (req: Request, res: Response) => {
-  try {
-    const user: User = await database('users').select().where({ id: req.params.id }).first();
-    if (user) {
-      await database('users').delete().where({ id: req.params.id });
-      res.sendStatus(204);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.post('/user', userController.create);
 
 //update
-app.put('/user/:id', async (req: Request, res: Response) => {
-  try {
-    const user: User = await database('users').select().where({ id: req.params.id }).first();
-    if (user) {
-      const newUser: User = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        age: req.body.age
-      }
-      await database('users').update(newUser).where({ id: req.params.id });
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.put('/user/:id', userController.update);
+
+//delete
+app.delete('/user/:id', userController.destroy);
+
 
 /*************GROUPS*************/
 
-interface Group {
-  id?: string,
-  name: string,
-  description: string,
-  location: string,
-  maximalSize: number
-  created_at?: string;
-  updated_at?: string;
-}
-
 //index
-app.get('/group', async (req: Request, res: Response) => {
-  const groups: Array<Group> = await database('groups').select();
-  res.json(groups);
-});
+app.get('/group', groupController.index);
 
 //show
-app.get('/group/:id', async (req: Request, res: Response) => {
-  try {
-    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
-    if (group) {
-      res.json(group);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.get('/group/:id', groupController.show);
 
 //create
-app.post('/group', async (req: Request, res: Response) => {
-  try {
-    const group: Group = {
-      name: req.body.name,
-      description: req.body.description,
-      location: req.body.location,
-      maximalSize: req.body.maximalSize
-    }
-    await database('groups').insert(group);
-    res.sendStatus(201);
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
-
-//delete
-app.delete('/group/:id', async (req: Request, res: Response) => {
-  try {
-    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
-    if (group) {
-      await database('groups').delete().where({ id: req.params.id });
-      res.sendStatus(204);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.post('/group', groupController.create);
 
 //update
-app.put('/group/:id', async (req: Request, res: Response) => {
-  try {
-    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
-    if (group) {
-      const newGroup: Group = {
-        name: req.body.name,
-        description: req.body.description,
-        location: req.body.location,
-        maximalSize: req.body.maximalSize
-      }
-      await database('groups').update(newGroup).where({ id: req.params.id });
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch(error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
+app.put('/group/:id', groupController.update);
+
+//delete
+app.delete('/group/:id', groupController.destroy);
+
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
