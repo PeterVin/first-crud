79022a6e87a817ab6ee8cd0a314781efb3efc6e2
Adanosz <reti925@gmail.com>
2020-04-23 11:24:01 +0200

    routers added

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,21 @@
 import * as express from 'express';
-import { Request, Response, Application } from 'express';
-import { database } from './lib/database';
-import {User} from './app/models/user';
-import {Group} from './app/models/group';
-import * as userController from './app/controllers/user'
-import * as groupController from './app/controllers/group'
+import { Application } from 'express';
+import {router as userRouter} from './app/routers/user'
+import {router as groupRouter} from './app/routers/group'
 
 const app: Application = express();
 app.use(express.json())
 const { PORT = 3000 } = process.env;
 
 
-app.get('/', async (req: Request, res: Response) => {
+/*app.get('/', async (req: Request, res: Response) => {
   res.json({
     message: 'hello world',
   });
-});
+});*/
 
-
-/*************USERS*************/
-
-//index
-app.get('/user', userController.index);
-
-//show
-app.get('/user/:id', userController.show);
-
-//create
-app.post('/user', userController.create);
-
-//update
-app.put('/user/:id', userController.update);
-
-//delete
-app.delete('/user/:id', userController.destroy);
-
-
-/*************GROUPS*************/
-
-//index
-app.get('/group', groupController.index);
-
-//show
-app.get('/group/:id', groupController.show);
-
-//create
-app.post('/group', groupController.create);
-
-//update
-app.put('/group/:id', groupController.update);
-
-//delete
-app.delete('/group/:id', groupController.destroy);
+app.use(userRouter);
+app.use(groupRouter);
 
 
 app.listen(PORT, () => {
